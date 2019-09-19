6663a5f2265dbc19220f4b651167b3bdae308ff9
Alex <alekarp11@gmail.com>
2019-09-19 10:25:58 +0300

    Refactor client

--- lib/bespokify/client.rb
+++ lib/bespokify/client.rb
@@ -1,14 +1,23 @@
+require 'bespokify/client/material'
+
 module Bespokify
   class Client
-    API_ENDPOINT = 'https://auth.bespokify.com/auth/realms/platform/protocol/openid-connect/token'.freeze
+    include HTTParty
+    include Bespokify::Client::Material
+    base_uri 'https://api.bespokify.com/v2'
+    format :json
 
     def initialize(login, password)
       @login = login
       @password = password
+      self.class.default_options.merge!(headers: { 'Authorization' => "Bearer #{auth_token}" })
     end
 
+    private
+
     def auth_token
-      response = HTTParty.post(API_ENDPOINT, body: { 'username': @login, 'password': @password, 'grant_type': 'password', 'client_id': 'public-api' }, headers: { 'Content-Type': 'application/x-www-form-urlencoded' })
+      auth_url = 'https://auth.bespokify.com/auth/realms/platform/protocol/openid-connect/token'
+      response = HTTParty.post(auth_url, body: { 'username': @login, 'password': @password, 'grant_type': 'password', 'client_id': 'public-api' }, headers: { 'Content-Type': 'application/x-www-form-urlencoded' })
       JSON.parse(response.body).dig('access_token')
     end
   end
