345d426d2adb0986cbbd1305631067836000d5cf
Alex <alekarp11@gmail.com>
2019-09-19 11:01:39 +0300

    Finish material part

--- lib/bespokify/client/material.rb
+++ lib/bespokify/client/material.rb
@@ -4,6 +4,22 @@ module Bespokify
       def materials(options = {})
         get('/materials', options)
       end
+
+      def material(material_id)
+        get("/materials/#{material_id}")
+      end
+
+      def create_material(options = {})
+        post('/materials', options)
+      end
+
+      def update_material(material_id, options = {})
+        patch("/materials/#{material_id}", options)
+      end
+
+      def delete_material(material_id)
+        delete("/materials/#{material_id}")
+      end
     end
   end
 end
